5749be2e91a1ca5c77993d80f753a7be3e1caa61
KenStarry <starrycodes@gmail.com>
2022-09-12 19:23:03 +0300

    Shoply E-Commerce login activity

--- app/src/main/java/com/example/shoply/MainActivity.kt
+++ app/src/main/java/com/example/shoply/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.shoply
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listeners()
+    }
+
+    private fun listeners() {
+
+        //  Send user to login activity
+        main_login_btn.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
