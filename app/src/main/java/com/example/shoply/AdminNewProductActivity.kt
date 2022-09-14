ddbf858b008a1ab4b089973157119d6496f1988f
KenStarry <starrycodes@gmail.com>
2022-09-14 19:45:06 +0300

    Shoply - Admin Panel (9)

---
+++ app/src/main/java/com/example/shoply/AdminNewProductActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shoply
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminNewProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_new_product)
+    }
+}
--- app/src/main/java/com/example/shoply/LoginActivity.kt
+++ app/src/main/java/com/example/shoply/LoginActivity.kt
@@ -32,6 +32,18 @@ class LoginActivity : AppCompatActivity() {
         login_btn.setOnClickListener {
             loginUser()
         }
+
+        admin_panel_link.setOnClickListener {
+
+            "Login Admin".also { login_btn.text = it }
+            "Admins".also { parentDbName = it }
+        }
+
+        not_admin_panel_link.setOnClickListener{
+
+            "Login".also { login_btn.text = it }
+            "Users".also { parentDbName = it }
+        }
     }
 
     //  login user from firebase
@@ -73,10 +85,23 @@ class LoginActivity : AppCompatActivity() {
 
                     if (usersData!!.phone == phone) {
                         if (usersData.password == password) {
-                            Toast.makeText(this@LoginActivity, "Logged in successfully!", Toast.LENGTH_SHORT).show()
 
-                            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-                            startActivity(intent)
+                            //  Check if the user is an admin or not
+                            if (parentDbName == "Admins") {
+
+                                Toast.makeText(this@LoginActivity, "Welcome Admin, you logged in successfully!", Toast.LENGTH_SHORT).show()
+
+                                val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                                startActivity(intent)
+
+                            } else if (parentDbName == "Users") {
+
+                                Toast.makeText(this@LoginActivity, "Logged in successfully!", Toast.LENGTH_SHORT).show()
+
+                                val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                                startActivity(intent)
+
+                            }
                         } else {
 
                             Toast.makeText(this@LoginActivity, "Incorrect Password", Toast.LENGTH_SHORT).show()
